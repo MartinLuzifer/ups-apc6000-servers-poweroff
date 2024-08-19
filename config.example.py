a63e30f2e9b892a74f57c6b166eeafd21e68133f
Освальд Мартин <raccoon-oswald@yandex.ru>
2024-08-19 11:47:40 +0500

    add_charge_limit

--- config.example.py
+++ config.example.py
@@ -1,5 +1,6 @@
 log_file = "/root/ups.log"
 ups_ip = ''
+charge_limit = 80
 
 snmp_get_battery = f'snmpwalk -v 1 -c public {ups_ip} 1.3.6.1.4.1.318.1.1.1.11.1.1.0'
 snmp_get_charge = f'snmpwalk -v 1 -c public {ups_ip} 1.3.6.1.4.1.318.1.1.1.2.2.1.0'
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 from defs import *
+from config import charge_limit
 
 powered_type = ups_powered_type()
 battery_charge = ups_charge()
 
 if (powered_type is not None) and (battery_charge is not None):
+
     match powered_type:
 
         case "line":
-            if battery_charge >= 75:
-                append_log(f"Battery charge: {battery_charge}% ; Powered type: {powered_type} \n")
-            if battery_charge < 70:
-                append_log(f"Battery charge: {battery_charge}% ; Powered type: {powered_type} (charged) \n")
+            if battery_charge <= 100:
+                append_log(f"Powered type: {powered_type} ; Battery charge: {battery_charge}% ; (Full) \n")
+            if battery_charge > 100:
+                append_log(f"Powered type: {powered_type} ; Battery charge: {battery_charge}% ; (Charged) \n")
 
         case "battery":
-            if battery_charge >= 75:
-                append_log(f'UPS APC 6000 Battery charge: {battery_charge}%, Powered type: {powered_type} \n')
-            if battery_charge < 75:
-                append_log(f'Battery charge: {battery_charge}% ; Powered type: {powered_type} \n shutdown system \n')
-                subprocess.getoutput("poweroff")
-
-
+            if battery_charge >= charge_limit:
+                append_log(f'Powered type: {powered_type} ; Battery charge: {battery_charge}%  \n')
+            if battery_charge < charge_limit:
+                append_log(f'Battery charge: {battery_charge}% ; Powered type: {powered_type} \n Shutdown system!!! \n')
+                output = subprocess.getoutput("poweroff")  # POWEROFF
+                append_log(output)
